4216f517dbb68230915800a928e07c7b689705ec
KwlsQ <vincentiussss@gmail.com>
2024-04-06 19:35:27 +0700

    render schemes selection

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const baseURL = "https://www.thecolorapi.com"
+const schemeModes = [
+    "monochrome", "monochrome-dark", "monochrome-light", "analogic", "complement", "analogic-complement", "triad", "quad"
+]
 
+const baseURL = "https://www.thecolorapi.com"
 const form = document.getElementById("form")
 
+// Render Schemes selection
+const renderSchemeSelection = () => {
+    let options = ''
+    schemeModes.forEach((mode) => {
+        options += `
+            <option value="${mode}">${mode}</option>
+        `
+    })
+    document.getElementById('scheme-mode').innerHTML = options
+}
+
+renderSchemeSelection()
+
+// Submit Event Listener
 document.addEventListener("submit", (e) => {
     e.preventDefault()
     const seedColorData = new FormData(form)
     const seedColor = seedColorData.get('seed-color')
     const schemeMode = seedColorData.get('scheme-mode')
-    console.log(seedColor)
-    console.log(schemeMode)
 })
+
+// Fetch
 const getColors = () => {
     fetch(`${baseURL}/id?hex=0047AB`)
         .then(response => response.json())
