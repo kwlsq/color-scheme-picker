0e5d2a7b198d8f4df820610f0f2f5d11c43d4dc5
KwlsQ <vincentiussss@gmail.com>
2024-04-06 19:24:05 +0700

    prepare inputs and getting the value from it

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const baseURL = "https://www.thecolorapi.com"
 
+const form = document.getElementById("form")
+
+document.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const seedColorData = new FormData(form)
+    const seedColor = seedColorData.get('seed-color')
+    const schemeMode = seedColorData.get('scheme-mode')
+    console.log(seedColor)
+    console.log(schemeMode)
+})
 const getColors = () => {
     fetch(`${baseURL}/id?hex=0047AB`)
-        .then(response=>response.json())
-        .then(data=>console.log(data))
+        .then(response => response.json())
+        .then(data => console.log(data))
 }
 
 const getSchemes = () => {
     fetch(`${baseURL}/scheme?hex=0047AB&mode=analogic`)
-        .then(response=>response.json())
-        .then(data=>console.log(data))
+        .then(response => response.json())
+        .then(data => console.log(data))
 }
